3187b78f0cf29fb677ee079b5beb278d96b8fa0f
sanya-shch <shchipkov.sanya@ukr.net>
2023-01-06 21:58:44 +0200

    added Header Component and Menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
+import React from "react";
+
+import "./App.css";
+
+import Header from "./components/header/Header";
+
+const App = () => {
   return (
     <div>
-
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
