1fba953b7f1528ed7b255bffe9564dfd45aecf41
sanya-shch <shchipkov.sanya@ukr.net>
2023-01-07 19:41:30 +0200

    added Profile block

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
 
       <main className="main">
         <Home />
+        <About />
       </main>
     </div>
   );
---
+++ src/components/about/Info.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./about.css";
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <i className="uil uil-award-alt about__icon"></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">4 Years Working</span>
+      </div>
+
+      <div className="about__box">
+        <i className="uil uil-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">6 Projects</span>
+      </div>
+
+      <div className="about__box">
+        <i className="uil uil-house-user about__icon"></i>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">30+ Projects</span>
+        <div className="about__subtitle">Non-Commercial</div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
