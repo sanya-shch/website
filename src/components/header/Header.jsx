d1f2481214c7db8f27ec8db63a504e7510afc388
sanya-shch <shchipkov.sanya@ukr.net>
2023-01-09 15:02:30 +0200

    updated Header block

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,53 +2,50 @@ import React, { useState } from "react";
 
 import "./header.css";
 
+const NAV_LIST = [
+  { name: "Home", id: "home" },
+  { name: "About", id: "about" },
+  { name: "Skills", id: "skills" },
+  { name: "Experience", id: "experience" },
+  { name: "Portfolio", id: "portfolio" },
+  { name: "Contact", id: "contact" },
+];
+
 const Header = () => {
+  window.addEventListener("scroll", function () {
+    const element = document.querySelector(".header");
+
+    if (this.scrollY >= 80) element.classList.add("scroll-header");
+    else element.classList.remove("scroll-header");
+  });
+
   const [toggle, showMenu] = useState(false);
+  const [active, setActive] = useState("#home");
 
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">
+        <a href="#" className="nav__logo">
           SHCHIPKOV
         </a>
 
         <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
-            <li className="nav__item">
-              <a href="#home" className="nav__link active-link">
-                <i className="uil uil-estate nav__icon"></i> Home
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#about" className="nav__link">
-                <i className="uil uil-user nav__icon"></i> About
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#skills" className="nav__link">
-                <i className="uil uil-file-alt nav__icon"></i> Skills
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#experience" className="nav__link">
-                <i className="uil uil-briefcase-alt nav__icon"></i> Experience
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#portfolio" className="nav__link">
-                <i className="uil uil-scenery nav__icon"></i> Portfolio
-              </a>
-            </li>
-
-            <li className="nav__item">
-              <a href="#contact" className="nav__link">
-                <i className="uil uil-message nav__icon"></i> Contact
-              </a>
-            </li>
+            {NAV_LIST.map((nav) => (
+              <li key={nav.id} className="nav__item">
+                <a
+                  href={`#${nav.id}`}
+                  onClick={() => setActive(`#${nav.id}`)}
+                  className={
+                    active === `#${nav.id}`
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  }
+                >
+                  <i className="uil uil-estate nav__icon"></i> {nav.name}
+                </a>
+              </li>
+            ))}
           </ul>
 
           <i
